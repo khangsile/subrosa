6ac48de3a1710d07dd1d71c52996da029db55178
khangsile <khangsile@gmail.com>
2012-12-26 12:57:06 -0500

    adding context menus and interface with online api

---
+++ store.js
@@ -0,0 +1,25 @@
+
+
+chrome.contextMenus.create({
+  "title": "Store Word", 
+  "contexts": ["selection"],
+  "onclick" : clickHandler
+  });
+
+var clickHandler = function(e) {
+  
+  if (e.selectionText) {
+    //add the word and definition to the database with timestamp
+      var req = new XMLHttpRequest();
+      req.open(
+	  "GET",
+	  "http://www.stands4.com/services/v2/defs.php?" +
+	  "uid=1001&" +
+	  "tokenid=tk324324&" +
+	  "word=" + e.selectionText,
+	   true);
+  } else {
+    //popup - "Incorrect input. Your argument is invalid."
+  }
+
+};
